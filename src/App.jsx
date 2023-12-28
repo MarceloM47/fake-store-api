aff674a87bbb77f675329058ef22dea5ece4bbc5
MarceloM47 <morinigomarcelo40@gmail.com>
2023-12-28 17:53:25 -0300

    Add react-query and zustand

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import './App.css'
-import Navbar from './components/Navbar'
-import Products from './components/products'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Routes, Route } from 'react-router-dom';
+
+import PrivateRoute from './components/PrivateRoute.jsx';
+import Navbar from './components/Navbar.jsx'
+import LoginPage from './pages/LoginPage.jsx';
+import ProductPage from './pages/HomePage.jsx';
 
 function App() {
+  const queryClient = new QueryClient();
+
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Navbar/>
-      <Products/>
-    </>
+      <Routes>
+          <Route element={<PrivateRoute />}>
+            <Route path="/" element={<ProductPage />} />
+          </Route>
+        <Route path="/login" element={ <LoginPage/> }/>
+      </Routes>
+    </QueryClientProvider>
   )
 }
 
