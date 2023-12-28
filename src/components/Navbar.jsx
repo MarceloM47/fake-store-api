604ba5143dffd12fd5a531a7739f4a572e78f154
MarceloM47 <morinigomarcelo40@gmail.com>
2023-12-28 17:55:37 -0300

    Modify the navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,40 @@
+import { Link, useNavigate  } from "react-router-dom"
+import { useAuthStore } from "../store/auth"
+
 function Navbar() {
+  const logout = useAuthStore(state => state.logout)
+  const token = useAuthStore.getState().token
+
+  const navigate  = useNavigate();
+
+  
   return (
     <nav className="navbar navbar-expand-lg bg-primary" data-bs-theme="dark">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">MarceloM47</a>
+            <Link to={'/'} className="navbar-brand">MarceloM47</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <Link to={'/'} className="nav-link active" aria-current="page">Home</Link>
                 </li>
-                <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
-                </li>
+                {
+                  token ? (
+                    <li className="nav-item">
+                      <a className="nav-link active" onClick={() => {
+                        logout()
+                        navigate('/login')
+                      }}>Logout</a>
+                    </li>
+                  ) : (
+                    <li className="nav-item">
+                      <Link to={'/login'} className="nav-link active">Login</Link>
+                    </li>
+                  )
+                }
             </ul>
-            <form className="d-flex" role="search">
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
             </div>
         </div>
     </nav>
