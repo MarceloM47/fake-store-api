c6d19beefdb32f4ef02b07b2442eb8536c810886
MarceloM47 <morinigomarcelo40@gmail.com>
2023-12-28 17:54:33 -0300

    Add the pages

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,12 @@
+import Login from "../components/Login"
+
+function LoginPage() {
+  return (
+    <section className="container w-100 h-100 mt-5 p-5">
+      <h1 className="text-center pb-5" style={{fontSize: "50px"}}>MarceloM47 Store</h1>
+      <Login/>
+    </section>
+  )
+}
+
+export default LoginPage
